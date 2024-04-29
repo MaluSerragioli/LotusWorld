79c53b8c1e87212e670be6142bbd3f1b2015ccca
MaluSerragioli <113370340+MaluSerragioli@users.noreply.github.com>
2024-04-29 18:51:59 -0300

    Add files via upload

---
+++ Login.js
@@ -0,0 +1,20 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyANewzzkPotk3yG7rNu6gf_n_2jWWa36oc",
+    authDomain: "batepapo-1f9f0.firebaseapp.com",
+    databaseURL: "https://batepapo-1f9f0-default-rtdb.firebaseio.com",
+    projectId: "batepapo-1f9f0",
+    storageBucket: "batepapo-1f9f0.appspot.com",
+    messagingSenderId: "549046197716",
+    appId: "1:549046197716:web:5783827cb05192e5c7ca41"
+};
+firebase.initializeApp(firebaseConfig)
+function Login() {
+    userName=document.getElementById("form2Example11").value
+    firebase.database().ref("/").child(userName).update({
+        purpose:"newUsername"
+    })
+    setTimeout(() => {
+        window.location = "LotusRoom.html"
+    }, 2000);
+    
+}
---
+++ LotusRoom.js
@@ -0,0 +1,29 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyANewzzkPotk3yG7rNu6gf_n_2jWWa36oc",
+    authDomain: "batepapo-1f9f0.firebaseapp.com",
+    databaseURL: "https://batepapo-1f9f0-default-rtdb.firebaseio.com",
+    projectId: "batepapo-1f9f0",
+    storageBucket: "batepapo-1f9f0.appspot.com",
+    messagingSenderId: "549046197716",
+    appId: "1:549046197716:web:5783827cb05192e5c7ca41"
+};
+firebase.initializeApp(firebaseConfig)
+
+function addRoom() {
+    roomName = document.getElementById("roomName").value;
+    firebase.database().ref("rooms/").child(roomName).update({ purpose: "addSala" })
+    localStorage.setItem("roomName", roomName)
+}
+function getData() {
+    firebase.database().ref("rooms/").on('value', function (snapshot) {
+        document.getElementById("output").innerHTML = "";
+        snapshot.forEach(function (childSnapshot) {
+            childKey = childSnapshot.key;
+            roomNames = childKey;
+            console.log("Nome da Sala - " + roomNames);
+            row = "<div class='roomName' id=" + roomNames + " onclick='redirectToRoomName(this.id)' >#" + roomNames + "</div><hr>";
+            document.getElementById("output").innerHTML += row;
+        });
+    });
+}
+getData()
